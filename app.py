0151d293767f70a919a180869c289f106be5d102
sivasubramaniyansahadevan-ops <sivasubramaniyansahadevan@gmail.com>
2026-06-23 19:59:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,21 @@ if uploaded_file:
 
     st.success("File Uploaded Successfully")
 
-    st.dataframe(df.head())
+    col1,col2,col3 = st.columns(3)
 
-    st.write("Total Projects:", len(df))
+    col1.metric(
+        "Projects",
+        len(df)
+    )
+
+    col2.metric(
+        "Average Risk Score",
+        round(df["risk_score"].mean(),2)
+    )
+
+    col3.metric(
+        "Average Delay",
+        round(df["schedule_variance_days"].mean(),2)
+    )
+
+    st.dataframe(df.head())
